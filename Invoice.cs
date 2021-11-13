27026c39616127d3fc00aa8ebdf8dabdacddf86f
ams6293 <amberspatig@mail.weber.edu>
2021-11-13 15:11:19 -0700

    added Invoice class  to create invoice object with getters and setters

---
+++ Invoice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3280groupProj
+{
+
+    /// <summary>
+    /// this class is the geters and setters for invoices
+    /// </summary>
+    class Invoice
+    {
+        public int InvoiceNum { get; set; }
+        public string InvoiceDate { get; set; }
+        public int TotalCost { get; set; }
+    }
+}

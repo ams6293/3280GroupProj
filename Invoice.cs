377c0227868c4b86a371998b1d968dffe3cbfcc7
ams6293 <amberspatig@mail.weber.edu>
2021-11-14 17:36:30 -0700

    Fixed the Invoice ToString override

--- Invoice.cs
+++ Invoice.cs
@@ -17,12 +17,30 @@ namespace _3280groupProj
         public int TotalCost { get; set; }
 
         /// <summary>
-        /// overrides tostring class to print invoicenum date and cost
+        /// overrides tostring class to print invoicenum date and cost 
         /// </summary>
-        /// <returns></returns>
+        /// <returns> returns the appropriate string based on what info is provided.</returns>
         public override string ToString()
         {
-            return $"{InvoiceNum} {InvoiceDate} {TotalCost}";
+            if(InvoiceNum == 0 && TotalCost == 0)
+            {
+                return $"{InvoiceDate}";
+            }
+            else if(InvoiceDate == null && TotalCost == 0)
+            {
+                return $"{InvoiceNum}";
+            }
+            else if(InvoiceDate == null && InvoiceNum == 0)
+            {
+                return $"{TotalCost}";
+            }
+            else
+            {
+                return $"{InvoiceNum} {InvoiceDate} {TotalCost}";
+            }
+
         }
+
+      
     }
 }
